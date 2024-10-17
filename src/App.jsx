bb2735e60488b287b7c5eaf5e3c11ac158c4e832
zafarbek <zafarbektolanboyev618@gmail.com>
2024-10-17 19:37:21 +0500

    firstComanda

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+function App() {
+  return (
+    <div>
+      <DeleteIcon></DeleteIcon>
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThreeDRotation></ThreeDRotation>
+    </div>
+  )
+}
+export default App;
